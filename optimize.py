21de8df6b85a0f3f613e8c4c64d3b79e043d71fe
camburd2 <cburdgic@umich.edu>
2025-01-10 17:57:25 -0500

    first commit

---
+++ optimize.py
@@ -0,0 +1,32 @@
+
+# Dimensions of available triangle region formed by sailboat mast and foredeck
+HEIGHT = 40
+L_BASE = 6
+
+COST_FRAME = 10 # cost per foot of panel frame [$/ft]
+COST_PANEL = 10 # cost per square foot of panel [$/ft^2]
+
+# Min/max panel width (same for all panels) [in]
+W_MIN = 12
+W_MAX = 36
+
+# Min/max panel spacing (same for all panels) [in]
+S_MIN = 12 
+S_MAX = 48
+
+# Min/max number of panels 
+N_MIN = 1
+N_MAX = 6
+
+
+# Variables: 
+#   N (num panels)
+#   S (panel spacing)
+#   W (panel width)
+
+# Params: 
+#   HEIGHT (max height)
+#   L_BASE (base panel length)
+#   COST_FRAME (cost/ft panel frame)
+#   COST_PANEL (cost/ft^2 panel)
+
