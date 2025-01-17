edae23dee9152d5b3ac36f75fc6858516bc231b8
camburd2 <cburdgic@umich.edu>
2025-01-16 22:20:52 -0500

    make boat length dynamic

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ class App:
         # initial stack'
         self._create_stack(
             num_panels=6,
-            panel_spacing=2,
-            panel_width=1
+            panel_spacing=3,
+            panel_width=2,
+            boat_length=40,
         )
 
         self.app = dash.Dash(__name__)
@@ -24,7 +25,7 @@ class App:
         self.setup_layout()
         self.setup_callbacks()
 
-    def _create_stack(self, num_panels, panel_spacing, panel_width):
+    def _create_stack(self, num_panels, panel_spacing, panel_width, boat_length):
         """Create a new active solar panel stack and static surfaces (panels, mast, deck)"""
         
         
@@ -32,20 +33,20 @@ class App:
             num_panels=num_panels,
             panel_spacing=panel_spacing,
             panel_width=panel_width,
-            boat_length=40
+            boat_length=boat_length
         )
 
         panels = self.active_stack.create_panel_surfaces()
 
         deck, mast_x = plotting.create_deck(
-            boat_length=40, 
+            boat_length=boat_length, 
             width=panel_width,
         )
 
         cylinder = plotting.create_cylinder(
             panel_width=panel_width,
             x_val=mast_x,
-            height=40
+            height=boat_length
         )
 
         self.static_surfaces = panels + [cylinder] + [deck]
@@ -59,19 +60,19 @@ class App:
                     html.H3("Panel Configuration", style={'margin-bottom': '50px'}),
                     html.Div(className='input-group', children=[
                         html.Label('Number of Panels', className='input-label'),
-                        dcc.Input(id='num-panels-input', type='number', value=5, min=1, className='input-field')
+                        dcc.Input(id='num-panels-input', type='number', value=6, min=1, className='input-field')
                     ]),
                     html.Div(className='input-group', children=[
                         html.Label('Panel Spacing (ft)', className='input-label'),
-                        dcc.Input(id='panel-spacing-input', type='number', value=1.5, step=0.1, className='input-field')
+                        dcc.Input(id='panel-spacing-input', type='number', value=3, step=0.1, className='input-field')
                     ]),
                     html.Div(className='input-group', children=[
                         html.Label('Panel Width (ft)', className='input-label'),
                         dcc.Input(id='panel-width-input', type='number', value=2, step=0.1, className='input-field')
                     ]),
                     html.Div(className='input-group', children=[
-                        html.Label('Panel Length (ft)', className='input-label'),
-                        dcc.Input(id='panel-length-input', type='number', value=6, step=0.1, className='input-field')
+                        html.Label('Boat Length (ft)', className='input-label'),
+                        dcc.Input(id='boat-length-input', type='number', value=40, step=1, className='input-field')
                     ]),
                     html.Div(id='estimated-power', className='estimate-power')
                 ]),
@@ -85,7 +86,7 @@ class App:
             # Bottom slider section
             html.Div(className='slider-container', children=[
                 html.Div(className='slider', children=[
-                    html.Label('Elevation (°)', className='slider-label'),
+                    html.Label('Sun Elevation (°)', className='slider-label'),
                     html.Div(className='slider-component', children=[
                         dcc.Slider(id='elevation-slider', min=0, max=90, value=45,
                                 marks={i: str(i) for i in range(0, 91, 15)},
@@ -93,7 +94,7 @@ class App:
                     ])
                 ]),
                 html.Div(className='slider', children=[
-                    html.Label('Azimuth (°)', className='slider-label'),
+                    html.Label('Sun Azimuth (°)', className='slider-label'),
                     html.Div(className='slider-component', children=[
                         dcc.Slider(id='azimuth-slider', min=0, max=360, value=180,
                                 marks={i: str(i) for i in range(0, 361, 30)},
@@ -141,16 +142,22 @@ class App:
             [Input('num-panels-input', 'value'),
             Input('panel-spacing-input', 'value'),
             Input('panel-width-input', 'value'),
+            Input('boat-length-input', 'value'),
             Input('elevation-slider', 'value'),
             Input('azimuth-slider', 'value')]
         )
-        def update_plot(num, spacing, width, heading, azimuth):
+        def update_plot(num, spacing, width, boat_len, heading, azimuth):
             ctx = dash.callback_context
             
             trigger = ctx.triggered[0]['prop_id']
-            if 'num-panels-input' in trigger or 'panel-spacing-input' in trigger or 'panel-width-input' in trigger:
+            if 'num-panels-input' in trigger or 'panel-spacing-input' in trigger or 'panel-width-input' in trigger or 'boat-length-input' in trigger:
                 # Inputs related to the stack
-                self._create_stack(num_panels=num, panel_spacing=spacing, panel_width=width)
+                self._create_stack(
+                    num_panels=num, 
+                    panel_spacing=spacing, 
+                    panel_width=width, 
+                    boat_length=boat_len
+                )
             
             #elif 'elevation-slider' in trigger or 'azimuth-slider' in trigger:
             #    # Inputs related to the sun position
--- layout.py
+++ layout.py
@@ -43,7 +43,6 @@ index_string = '''
             .slider-container {
                 padding: 20px;
                 background-color: white;
-                box-shadow: 0 -2px 5px rgba(0,0,0,0.1);
             }
             .slider {
                 display: flex;
